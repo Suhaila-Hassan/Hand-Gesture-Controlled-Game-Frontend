b1fa54a442796a6308d3f22e4be00a273fdcc490
Suhaila Hassan <140956614+Suhaila-Hassan@users.noreply.github.com>
2025-06-01 13:56:00 +0300

    Update api-call.js

--- api-call.js
+++ api-call.js
@@ -2,7 +2,7 @@ async function getPredictedLabel(landmarks) {
   const landmarkData = landmarks.map(({ x, y }) => [x, y]);
 
   try {
-    const response = await fetch("http://localhost:8000/predict", {
+    const response = await fetch("https://hand-gesture-controlled-game-suhailahassan.up.railway.app/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
